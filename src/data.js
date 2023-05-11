f25c424d3aad2d1b9576fc5443f774cb7c978a19
CYNTHIAMOR <kabita262011@hotmail.com>
2023-05-10 23:41:19 -0500

    agreguehover,centre,subidataporarreglar

--- src/data.js
+++ src/data.js
@@ -1,9 +1,25 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+function filter(data, propiedad){
+  return data.filter(athletes =>{
+    athletes.sport === propiedad
+  });
+  }
+  
+  export const dataAthletes = (data, propiedad) => {
+    return 'data.propiedad';
+  
+  };
+  
+  export const name = () => {
+    return 'name';
+  }
+  
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+//export const example = () => {
+  //return 'example';
+//};
+
+//export const anotherExample = () => {
+  //return 'OMG';
+//};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
-import { example } from './data/athletes/athletes.js'
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import { example } from './data/athletes/athletes.js';
+//import data from './data/athletes/athletes.js';
+//import data from './data/athletes/athletes.js';
+import { dataAthletes, dataAthletes } from './data.js';
+import dataAthletes from './data/athletes/athletes.js';
 
-console.log(example, data);
+
+
+Object.keys(data.athletes).forEach((gender) => {
+    const element = data.athletes[gender];
+    console.log([element.gender]);
+  });
+ for (let i = 0; i < data.athletes.length; i++){
+ console.log([data.athletes[i].name]);
+ }
+
+ const data = document.getElementById(data.athletes)
+const botondeportes = document.getElementById(data.boton-deportes)
+
+botondeportes.addEventListener("click", function(){
+})
+
+
+//console.log(example, data);
