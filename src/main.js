71cd9cd53c961d6d5e75428bfb30913e02a6db76
Cris-hr <cjminisol@gmail.com>
2023-05-12 12:03:19 -0500

    data subida

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(data.pokemon);
+
+const contenidoCard = document.querySelector(".contenidoCard");
+//const informaCard = document.getElementById("info");
+const showCard = document.querySelectorAll("#showCard");
+
+
+data.pokemon.forEach((i,idx) => {
+  contenidoCard.innerHTML += `
+    <button class= "todo" id="${showCard}">
+    <div class= "card-contenido">
+    <p class="numeroPokemon" >${i.num}</p>
+    <h3>${i.name}</h3>
+    <div class="power">
+    ${i.type}
+    </div>
+    </div>
+    <div class="card-img">
+    <img src="${i.img}" class= "imagen" alt="">
+    </div>
+    </button>`;
+})
