755c1b30032a309aa5a73c49d474590246f25729
Ana Laura Vargas Piña <analauvapi15@gmail.com>
2023-05-16 16:34:17 -0600

    recuperando compu

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { example } from './data/athletes/athletes.js'
+import { data } from './data/athletes/athletes.js'
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
