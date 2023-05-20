cffb70802bbd1059e06c019d3f38397d4bc204cf
Ana Laura Vargas Piña <analauvapi15@gmail.com>
2023-05-20 09:15:15 -0600

    actualizacion funcion filtrar por deporte

--- src/data.js
+++ src/data.js
@@ -1,17 +1,1 @@
-/*export { dataAthletesFilter }
-const dataAthletesFilterSport = () => {
-return 'propiedad'= dataAthletesFilter
-}export { dataAthletesFilter }
-const dataAthletesFilterTeam = () => {
-return 'propiedad'= dataAthletesFilter
-}
-export { dataAthletesFilter }
-const dataAthletesFilterName= () => {
-return 'propiedad'= dataAthletesFilter
-}
 
-export filterSport(data, deporte){
-const deportes = data.filter(deportes =>deportes.sport===deporte)
-  return deportes
-}
-*/
