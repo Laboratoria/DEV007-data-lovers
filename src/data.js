5cfbd2d3c93d3860eb0e2248fa3ff1b58e421b7a
Anggie Flores <prueba@correo.com>
2023-05-26 15:43:13 -0500

    pegue codigo de repositorio

--- src/data.js
+++ src/data.js
@@ -1,9 +1,16 @@
-// estas funciones son de ejemplo
+// // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+// export const example = () => {
+//   return 'example';
+// };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+
+export const filterDataByType= (data, condition) =>{
+  const pokemonesFiltrados= data.filter((pokemon) => pokemon.type.includes(condition));
+  console.log(pokemonesFiltrados)
+  return pokemonesFiltrados;
+  }
+  
