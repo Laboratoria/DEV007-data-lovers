518e751ddf0e62431a1cbadad920d0ab99c1d413
CYNTHIAMOR <kabita262011@hotmail.com>
2023-05-29 10:06:16 -0500

    ok

--- src/data.js
+++ src/data.js
@@ -8,6 +8,7 @@ export function filtrarPaises (data) {
 
 export function filtrarDeportes (data) {
   const deportesUnicos = new Set()
+
   data.athletes.forEach(i => {
     deportesUnicos.add(i.sport)
   })
@@ -29,7 +30,7 @@ export function filtrarAtletasPorDeporte (athletes, deporte) {
 export function ordenarAtletas (atletas, ordenSeleccionado) {
   const atletasOrdenados = atletas.slice() // Crear una copia de la lista de atletas con .slice
   atletasOrdenados.sort((a, b) => {
-    //con .sort vamos a ordenar todo lo que este dentre de los {}
+    //con .sort vamos a ordenar todo lo que este dentro de los {}
     const nombreA = a.name.toLowerCase() //asegurarnos que la comparacion sea insensible a mayusculas y minusculas
     const nombreB = b.name.toLowerCase()
     if (ordenSeleccionado === 'AZ') {
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,43 @@
-import { example, anotherExample } from '../src/data.js';
-import data from './data/athletes/athletes.js';
+import { filtrarPaises, filtrarDeportes , filtrarAtletasPorPais , filtrarAtletasPorDeporte , ordenarAtletas, sortData} from '../src/data.js';
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+//testear que filtrarPaises es una funcion
+describe('filtrarPaises', () => {
+  it('devuelve un conjunto vacío si no hay datos de atletas', () => {
+    expect(typeof filtrarPaises).toBe('function');
   });
 });
-
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+//test A - Z
+test('Debería ser una función')
+describe('Aplicación de tests para función ordenarAtletas', (atletas, ordenSeleccionado) => {
+  it('El return de la función ordenarAtletas es de tipo: Objeto', () => {
+    expect(typeof sortData(ordenarAtletas, 'ASC')).toBe('object')
+  })
+describe('Aplicación de tests para función sortData', () => {
+  it('El return de la función sortData es de tipo: Objeto', () => {
+    expect(typeof sortData(testData, 'ASC')).toBe('object');
   });
+}));
+import { filterCoaches, findCDP } from '../src/data.js';
+import data from './../src/data/dev007/dev007.js'
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+
+describe('filtrarPaises', () => {
+  test('Debería ser una función.', () => {
+    expect(typeof filtrarPaises).toBe('function');
+    
+  })
+
+  it('Filtra y devuelve a los paises.', () => {
+    expect(filtrarPaises(data.athletes)).toEqual
+  })
+
+  it('Deberían ser ', () => {
+    expect(filterCoaches(data.personas)).toHaveLength(3)
+  })
+})
+
+describe('findCDP', () => {
+  it('Debería devolverme a la CDP (Nicoooooooooooooooooooooooooooooooooooo :)).', () => {
+    expect(findCDP(data.personas)).toHaveProperty('nombre', 'Nico');
+  })
+})
